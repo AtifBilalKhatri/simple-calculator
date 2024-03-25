df9080bc374a2746949250cff20b8e6bd006942b
Atif Bilal Khatri <164232390+AtifBilalKhatri@users.noreply.github.com>
2024-03-26 02:52:10 +0500

    my first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number ", type: "number", name: "firstNumber" },
+    { message: "endter second number ", type: "number", name: "secondNumber" },
+    {
+        message: "select any operator to perform opertaion ",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "substraction", "multiplication", "division"],
+    },
+]);
+// conditional statements
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("you use wrong operator");
+}
